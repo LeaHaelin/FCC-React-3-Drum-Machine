acaacd5b556c9a4bf32d1a180ae1b2245dce497e
LeaHaelin <lea8haelin@gmail.com>
2023-02-22 00:10:39 +0100

    refactor slider

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
         <h1 className="drum-box__title">Drum Machine</h1>
         <Drum volume={volume} />
         <div className="volume-control">
-          <img className="volum-control__icon down" src={volumeDown} alt="" />
+          <img className="volume-control__icon down" src={volumeDown} alt="" />
           <input className="volume-control__bar" id="slider" type="range" value={volume} step="0.1" max="1" min="0" onChange={(e) => setVolume(e.target.value)} />
-          <img className="volum-control__icon up" src={volumeUp} alt="" />
+          <img className="volume-control__icon up" src={volumeUp} alt="" />
         </div>
       </main>
     </div>
